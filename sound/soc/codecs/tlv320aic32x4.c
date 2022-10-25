0241d679cd1ae0dd5fc76ebacec9f0929036b7bf
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2022-10-25 13:32:35 +0200

    ASoC: tlv320aic32x4: switch to using gpiod API
    Switch the driver from legacy gpio API that is deprecated to the newer
    gpiod API that respects line polarities described in ACPI/DT.
    
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- sound/soc/codecs/tlv320aic32x4.c
+++ sound/soc/codecs/tlv320aic32x4.c
@@ -13,9 +13,9 @@
 #include <linux/moduleparam.h>
 #include <linux/init.h>
 #include <linux/delay.h>
+#include <linux/err.h>
 #include <linux/pm.h>
-#include <linux/gpio.h>
-#include <linux/of_gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/cdev.h>
 #include <linux/slab.h>
 #include <linux/clk.h>
@@ -41,7 +41,7 @@ struct aic32x4_priv {
 	u32 power_cfg;
 	u32 micpga_routing;
 	bool swapdacs;
-	int rstn_gpio;
+	struct gpio_desc *reset_gpio;
 	const char *mclk_name;
 
 	struct regulator *supply_ldo;
@@ -1230,7 +1230,6 @@ static int aic32x4_parse_dt(struct aic32x4_priv *aic32x4,
 
 	aic32x4->swapdacs = false;
 	aic32x4->micpga_routing = 0;
-	aic32x4->rstn_gpio = of_get_named_gpio(np, "reset-gpios", 0);
 
 	if (of_property_read_u32_array(np, "aic32x4-gpio-func",
 				aic32x4_setup->gpio_func, 5) >= 0)
@@ -1365,16 +1364,16 @@ int aic32x4_probe(struct device *dev, struct regmap *regmap)
 		aic32x4->power_cfg = 0;
 		aic32x4->swapdacs = false;
 		aic32x4->micpga_routing = 0;
-		aic32x4->rstn_gpio = -1;
 		aic32x4->mclk_name = "mclk";
 	}
 
-	if (gpio_is_valid(aic32x4->rstn_gpio)) {
-		ret = devm_gpio_request_one(dev, aic32x4->rstn_gpio,
-				GPIOF_OUT_INIT_LOW, "tlv320aic32x4 rstn");
-		if (ret != 0)
-			return ret;
-	}
+	aic32x4->reset_gpio = devm_gpiod_get_optional(dev, "reset",
+						      GPIOD_OUT_HIGH);
+	ret = PTR_ERR_OR_ZERO(aic32x4->reset_gpio);
+	if (ret)
+		return ret;
+
+	gpiod_set_consumer_name(aic32x4->reset_gpio, "tlv320aic32x4 rstn");
 
 	ret = aic32x4_setup_regulators(dev, aic32x4);
 	if (ret) {
@@ -1382,9 +1381,9 @@ int aic32x4_probe(struct device *dev, struct regmap *regmap)
 		return ret;
 	}
 
-	if (gpio_is_valid(aic32x4->rstn_gpio)) {
+	if (aic32x4->reset_gpio) {
 		ndelay(10);
-		gpio_set_value_cansleep(aic32x4->rstn_gpio, 1);
+		gpiod_set_value_cansleep(aic32x4->reset_gpio, 0);
 		mdelay(1);
 	}
 
